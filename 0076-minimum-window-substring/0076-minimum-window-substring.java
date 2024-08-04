1b10057ca53290100dbedbad2b6a740c60d7d2da
Harsh Pratap <118646432+Harshu0516@users.noreply.github.com>
2024-08-04 18:24:33 +0530

    Time: 3 ms (83.23%), Space: 44.9 MB (48.85%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String minWindow(String s, String p) {
+        int n=s.length();
+        int m=p.length();
+        // int len=0;
+        int len = Integer.MAX_VALUE;
+        int L=0;
+        int R=0;
+        int si=-1;
+        int hash[]=new int[256];
+        for(int i=0;i<m;i++){
+            hash[p.charAt(i)]++;
+        }
+        int c=0;
+        while(R<n){
+            char ch=s.charAt(R);
+            if(hash[ch]>0){
+                c++;
+                
+            }
+            hash[ch]--;
+            while(c==m){
+                if(R-L+1<len){
+                    len=R-L+1;
+                    si=L;
+                }
+                hash[s.charAt(L)]++;
+                if(hash[s.charAt(L)]>0) c--;
+
+                L++;
+            }
+            R++;
+
+
+        }
+        return si == -1 ? "" : s.substring(si, si + len);
+    }
+}
